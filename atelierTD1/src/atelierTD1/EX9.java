4ac58a075317892055354155e95bd75138f0e6c5
nourg <nourg@NOURGUESMI.lan>
2025-12-19 22:22:15 +0100

    EX9

--- atelierTD1/src/atelierTD1/EX9.java
+++ atelierTD1/src/atelierTD1/EX9.java
@@ -1,5 +1,29 @@
 package atelierTD1;
-
+import java.util.Scanner;
 public class EX9 {
+	
 
-}
+	
+	    public static void main(String[] args) {
+
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Donner X : ");
+	        int x = sc.nextInt();
+
+	        System.out.print("Donner n : ");
+	        int n = sc.nextInt();
+
+	        int result = 1;
+	        for (int i = 0; i < n; i++) {
+	            result *= x;
+	        }
+
+	        System.out.println(x + " puissance " + n + " = " + result);
+
+	        sc.close();
+	    }
+	}
+
+
+

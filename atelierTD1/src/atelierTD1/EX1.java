e19b36bef34b73b6a9ec27f9828e1e083ea2ccc3
nourg <nourg@NOURGUESMI.lan>
2025-12-19 21:34:00 +0100

    EX1

---
+++ atelierTD1/src/atelierTD1/EX1.java
@@ -0,0 +1,13 @@
+package atelierTD1;
+
+public class EX1 {
+	
+	    public static void main(String[] args) {
+	        System.out.println("Je suis un génie");
+	        System.out.println();
+	        System.out.println("Je sais écrire un programme \"JAVA\"");
+	    }
+	
+
+
+}

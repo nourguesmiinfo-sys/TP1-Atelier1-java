a8fee0038afbe19a8cbde4b975d8377467243803
nourg <nourg@NOURGUESMI.lan>
2025-12-19 22:04:45 +0100

    EX2

---
+++ atelierTD1/src/atelierTD1/EX2.java
@@ -0,0 +1,16 @@
+package atelierTD1;
+
+public class EX2 {
+	
+	    public static void main(String[] args) {
+	        float moyenne = 10.5f;
+
+	        System.out.println(25 + 25);
+	        System.out.println("25" + 25);
+	        System.out.println("Mohamed" + 1);
+	        System.out.println("La moyenne" + " de la classe=" + moyenne + 2);
+	    }
+	}
+
+
+
